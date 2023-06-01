b7dadb5f26a6fc2a3cbae48127809b7829938865
samet <samettaspinarx@gmail.com>
2023-06-01 18:23:10 -0400

    [UPD] main updated

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from utils.pdf_utils import convert_pdf_to_txt
-
+from utils.resume_utils import get_resume_as_text
+from utils.job_description_utils import jd_main
 
 file = '/users/samettaspinar/desktop/resumes/resume2.pdf'
+jd_file = '/users/samettaspinar/desktop/jd/cellino_jd.pdf'
 
 
-print(convert_pdf_to_txt(file))
+# print(get_resume_as_text(file))
+print(jd_main(jd_file))
