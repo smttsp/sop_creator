9ab6d9c403a498ef71161db31872f01c8757bbe6
samet <samettaspinarx@gmail.com>
2023-06-01 21:15:01 -0400

    [UPD] improved `remove_extra_spaces`

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -1,8 +1,13 @@
 
 
-def remove_extra_spaces(string):
+def remove_extra_spaces(a_str):
     double_space = "  "
-    while double_space in string:
-        string = string.replace(double_space, " ")
+    double_line = "\n\n"
 
-    return string
+    while double_space in a_str:
+        a_str = a_str.replace(double_space, " ")
+
+    while double_line in a_str:
+        a_str = a_str.replace(double_line, "\n")
+
+    return a_str
