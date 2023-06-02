04fa383175904e1f020e6de51a3649ea674a013b
samet <samettaspinarx@gmail.com>
2023-06-02 18:09:33 -0400

    [UPD] jd_link updated. it is now a form

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+from main import get_content_from_inputs
 
 
 app = Flask(__name__)
@@ -13,10 +14,16 @@ def index():
 def process():
     resume_file = request.files["resume"]
     jd_file = request.files["jd"]
+    jd_link = request.form["jd_link"]
+    # jd_link = ""
+    # print(jd_file)
     # Process the uploaded file here and generate a reply
     reply = "Resume filename: " + resume_file.filename + "<br>"
-    reply += "JD filename: " + jd_file.filename
-    return reply
+    reply += "JD filename: " + jd_file.filename + "<br>"
+    reply += jd_link
+
+    content = reply + "<br><br>" + get_content_from_inputs(resume_file, jd_file, jd_link)
+    return content
 
 
 if __name__ == "__main__":
