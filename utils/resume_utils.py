ceecf66db6243840fd0e4c0fb41cb7e82fd501ab
samet <samettaspinarx@gmail.com>
2023-06-01 21:14:43 -0400

    [UPD] `convert_docx_to_text` added

--- utils/resume_utils.py
+++ utils/resume_utils.py
@@ -1,12 +1,11 @@
 from utils.pdf_utils import convert_pdf_to_txt
 from utils.string_utils import remove_extra_spaces
+from utils.word_utils import convert_docx_to_text
 
 
 def get_resume_as_text(file):
     all_resume = convert_pdf_to_txt(file)
-    if all_resume is None:
-        # try word
-        pass
+    all_resume = convert_docx_to_text(file) if all_resume is None else all_resume
 
     if isinstance(all_resume, str):
         all_resume = remove_extra_spaces(all_resume)
