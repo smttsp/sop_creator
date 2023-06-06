850777f88ac72d4d7772bde4f91568a0c7708602
samet <samettaspinarx@gmail.com>
2023-06-06 12:15:27 -0400

    [UPD] removing emails and phones from resumes incorparated

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,5 +1,5 @@
 from utils.pdf_utils import convert_pdf_to_txt
-from utils.string_utils import remove_extra_spaces
+from utils.string_utils import remove_extra_spaces, find_emails, find_phone_numbers, anonymize_text
 from utils.word_utils import convert_docx_to_text
 
 
@@ -16,7 +16,12 @@ def read_text_from_file(filename):
     all_resume = convert_pdf_to_txt(filename)
     all_resume = convert_docx_to_text(filename) if all_resume is None else all_resume
 
+    emails = find_emails(all_resume)
+    phone_numbers, formatted_numbers = find_phone_numbers(all_resume)
+
     if isinstance(all_resume, str):
+        all_resume = anonymize_text(all_resume, emails, "")
+        all_resume = anonymize_text(all_resume, phone_numbers, "")
         all_resume = remove_extra_spaces(all_resume)
 
     return all_resume
