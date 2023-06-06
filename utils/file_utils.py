0e3561626823ec5b5053191778a9a8bf2cec0756
samet <samettaspinarx@gmail.com>
2023-06-06 16:01:05 -0400

    [DOC] docstrings for file utils added

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -7,33 +7,63 @@ import orjson
 
 
 def split_file_path(file_path):
+    """Split a file path into its parent directory, base filename, and extension.
+
+    Args:
+        file_path (str): The path of the file.
+
+    Returns:
+        tuple: A tuple containing the parent directory, filename without extension, and file extension.
+    """
+
     parent_dir = os.path.dirname(file_path)
 
-    # Get the base filename
     base_filename = os.path.basename(file_path)
 
-    # Split the base filename and extension
     filename, file_extension = os.path.splitext(base_filename)
     return parent_dir, filename, file_extension
 
 
 def save_orjson(filepath, data):
+    """Save data as a JSON file using orjson.
+
+    Args:
+        data: The data to be saved as a JSON file.
+        file_path (str): The path of the file where the JSON data will be saved.
+
+    Raises:
+        OSError: If there is an error while opening or writing to the file.
+    """
+
     with open(filepath, "wb") as file:
         file.write(orjson.dumps(data))
 
 
 def save_files(folder, resume_file, jd_file, content_dict):
+    """Save files to a specified folder, including resume, job description, and content dictionary.
+
+    Args:
+        folder (str): The path of the destination folder where the files will be saved.
+        resume_file (str): The path of the resume file to be saved.
+        jd_file (str): The path of the job description file to be saved.
+        content_dict (dict): The content dictionary to be saved as a JSON file.
+
+    Returns:
+        None
+    """
+
     *_, resume_extension = split_file_path(resume_file)
     *_, jd_extension = split_file_path(jd_file)
+
     resume_dst_path = os.path.join(folder, "resume" + resume_extension)
     jd_dst_path = os.path.join(folder, "jd" + resume_extension)
     content_dst_path = os.path.join(folder, "content.json")
+
     shutil.copyfile(resume_file, resume_dst_path)
     shutil.copyfile(jd_file, jd_dst_path)
-
     save_orjson(content_dst_path, content_dict)
 
-    pass
+    return None
 
 
 def read_text_from_file(filename):
