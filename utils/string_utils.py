3111f66677341375488c80395d0dc06c29ef75c0
samet <samettaspinarx@gmail.com>
2023-06-06 12:12:36 -0400

    [UPD] find phone numbers

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -1,11 +1,31 @@
 def remove_extra_spaces(a_str):
     double_space = "  "
     double_line = "\n\n"
+def find_phone_numbers(text):
+    phone_patterns = [
+        # r'\b\d{3}[-.]?\d{3}[-.]?\d{4}\b',  # xxx-xxx-xxxx or xxxxxxxxxx
+        # r'\b\(\d{3}\)[-.\s]\d{3}[-.\s]?\d{4}\b',  # (xxx) xxx-xxxx or (xxx)xxx-xxxx or (xxx) xxx xxxx
+        # r'\b\d{3}\s\d{3}\s\d{4}\b',  # xxx xxx xxxx
+        # r'\b\d{3}[-.]?\d{4}[-.]?\d{4}\b',  # xxx-xxxx-xxxx or xxxxxxxxxxxx
+        # r'\b\(\d{3}\)\s\d{3}-\d{4}\b',  # (xxx) xxx-xxxx
+        r"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}",
+    ]
 
-    while double_space in a_str:
-        a_str = a_str.replace(double_space, " ")
+    # phone_patterns = [
+    #     r'\b\+?\d{1,3}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}\b',  # with country code and various separators
+    #     r'\b\+?\d{1,3}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}\b',  # with country code and various separators, without extension
+    #     r'\b\+?\d{1,3}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,9}\b',  # with country code and various separators, without area code
+    #     r'\b\+?\d{1,3}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}\b',  # with country code and various separators, without area code and extension
+    #     r'\b\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}\b',  # without country code, with various separators
+    #     r'\b\d{1,4}[-.\s]?\d{1,4}\b',  # without country code, with various separators, without extension
+    #     r'\b\d{1,9}\b'  # without country code, without area code or separators
+    # ]
 
-    while double_line in a_str:
-        a_str = a_str.replace(double_line, "\n")
+    phone_numbers = []
+    for pattern in phone_patterns:
+        phone_numbers += re.findall(pattern, text)
 
-    return a_str
+    formatted_numbers = standardize_phone_numbers(phone_numbers)
+    unique_phone_numbers = list(set(formatted_numbers))
+    return phone_numbers, unique_phone_numbers
+
