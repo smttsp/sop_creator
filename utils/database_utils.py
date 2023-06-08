85ed61fd941c5730c8a0b8b3ccabe028bf01b09b
Samet <samettaspinarx@gmail.com>
2023-06-08 15:02:39 -0400

    ML-52 [UPD] connect_to_db func added

---
+++ utils/database_utils.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+
+def connect_to_db(db_login_info_dict):
+    conn = psycopg2.connect(
+        host=db_login_info_dict["host"],
+        port=db_login_info_dict["port"],
+        database=db_login_info_dict["database"],
+        user=db_login_info_dict["user"],
+        password=db_login_info_dict["password"]
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM "user";')
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
+
+    return conn
--- utils/secret_manager_utils.py
+++ utils/secret_manager_utils.py
@@ -3,8 +3,9 @@ import json
 from google.cloud import secretmanager
 
 
-def get_secret_value_dict(project_id ="818082405938", secret_id ="cover_letter", version_id ="latest"):
-
+def get_secret_value_dict(
+    project_id="818082405938", secret_id="cover_letter", version_id="latest"
+):
     client = secretmanager.SecretManagerServiceClient()
 
     secret_name = (
