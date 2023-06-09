8ed7eb99f57f89ee236d5f7807c4be2b581d0ca8
Samet <samettaspinarx@gmail.com>
2023-06-09 16:05:39 -0400

    ML-53 [CLN] removed pulling rows from database

--- utils/database_utils.py
+++ utils/database_utils.py
@@ -9,15 +9,13 @@ def connect_to_db(db_login_info_dict):
         user=db_login_info_dict["user"],
         password=db_login_info_dict["password"]
     )
-
-    cursor = conn.cursor()
-
-    cursor.execute('SELECT * FROM "user";')
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-
-    cursor.close()
-    conn.close()
-
+    # cursor = conn.cursor()
+    #
+    # cursor.execute('SELECT * FROM "user";')
+    # rows = cursor.fetchall()
+    # for row in rows:
+    #     print(row)
+    #
+    # cursor.close()
+    # conn.close()
     return conn
