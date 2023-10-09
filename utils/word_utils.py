15c7645ee026b7cafc35adc1ce21e2994e4f5f0e
smttsp <samettaspinarx@gmail.com>
2023-10-09 19:34:17 -0400

    [UPD] improved stopwords

--- utils/word_utils.py
+++ utils/word_utils.py
@@ -1,6 +1,7 @@
 import docx2txt
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+from utils.stop_word_list import STOPWORDS
 
 
 def convert_docx_to_text(file):
@@ -12,11 +13,18 @@ def convert_docx_to_text(file):
     return text
 
 
-def get_word_cloud(content):
+def get_word_cloud(content, stopwords=None):
+    if stopwords is None:
+        stopwords = STOPWORDS
+
     wordcloud_obj = WordCloud(
-        width=1000, height=500, max_words=20, min_word_length=2
+        width=1000,
+        height=500,
+        max_words=20,
+        min_word_length=2,
+        stopwords=stopwords,
     )
-    wordcloud = wordcloud_obj.generate(content)
+    wordcloud = wordcloud_obj.generate(content.lower())
     plt.figure(figsize=(15, 8))
     plt.imshow(wordcloud)
     plt.axis("off")
