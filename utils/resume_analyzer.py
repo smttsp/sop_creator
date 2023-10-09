36caeb0c7105c9b0ac5db07119c966fe4a25d9aa
smttsp <samettaspinarx@gmail.com>
2023-10-09 18:34:48 -0400

    [UPD] resume analyzer class is created

---
+++ utils/resume_analyzer.py
@@ -0,0 +1,13 @@
+
+
+
+class ResumeAnalyzer:
+    def __init__(self, resume, job_description):
+        self.resume = resume
+        self.job_description = job_description
+
+    def get_matching_score(self):
+        pass
+
+    def get_word_clouds(self):
+        pass
--- utils/word_utils.py
+++ utils/word_utils.py
@@ -1,4 +1,6 @@
 import docx2txt
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 
 def convert_docx_to_text(file):
@@ -8,3 +10,13 @@ def convert_docx_to_text(file):
         text = None
 
     return text
+
+
+def get_word_cloud(content):
+    #convert list to string and generate
+    wordcloud = WordCloud(width=1000, height=500).generate(content)
+    plt.figure(figsize=(15, 8))
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.show()
+    
