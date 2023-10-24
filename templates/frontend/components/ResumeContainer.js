2a9d42aaf25afe2ca926dc6d9ed88258ae82dda0
bereketli <blingerew2018@gmail.com>
2023-10-24 17:49:15 +0300

    [FE] enlarge the size of resume/text container

--- templates/frontend/components/ResumeContainer.js
+++ templates/frontend/components/ResumeContainer.js
@@ -1,5 +1,5 @@
 export default function ResumeContainer({children}){
-    return <div className="bg-opacity-80  h-96 shadow-xl p-4 rounded-xl pt-8 ">
+    return <div className="bg-opacity-80  h-screen shadow-xl p-4 rounded-xl pt-8 ">
 
             {children}
     </div>
