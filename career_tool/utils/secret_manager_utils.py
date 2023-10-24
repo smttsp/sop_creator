ee2a1ce4dc6638040dfb404b04bfda3ee940b6f2
Samet <samettaspinarx@gmail.com>
2023-10-24 08:32:52 -0400

    [UPD] secret manager added

--- career_tool/utils/secret_manager_utils.py
+++ career_tool/utils/secret_manager_utils.py
@@ -4,9 +4,13 @@ from google.cloud import secretmanager
 
 
 def get_secret_value_dict(
-    project_id="818082405938", secret_id="cover_letter", version_id="latest"
+    service_account_key_path, project_id="984615452457", secret_id="secret1", version_id="latest"
 ):
-    client = secretmanager.SecretManagerServiceClient()
+
+    # client = secretmanager.SecretManagerServiceClient()
+    client = secretmanager.SecretManagerServiceClient.from_service_account_json(
+        service_account_key_path
+    )
 
     secret_name = (
         f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
