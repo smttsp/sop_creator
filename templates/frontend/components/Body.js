3ef4ae66b1819ec86ba97e82cb8b185df9abc3cb
bereketli <blingerew2018@gmail.com>
2023-10-25 11:17:04 +0300

    [FE] analysed-result showing page

--- templates/frontend/components/Body.js
+++ templates/frontend/components/Body.js
@@ -4,11 +4,16 @@ import Jobds from './Jobds';
 import Button from './Button';
 import Recomendation from './Recomendation';
 import ResumeContainer from './ResumeContainer';
+import ButtonSubmit from './ButtonSubmit';
+import Result from './Result';
+import AnalysisContainer from './AnalysisContainer';
+import SignificantTerms from './SignificantTerms';
 
 export default function Body() {
   const [fileText, setFileText] = useState('');
-  const [resume, setResume] = useState('')
- 
+  const [resume, setResume] = useState('');
+  const [showResult, setShowResult] = useState(false);
+
   const handleResume = (text) => {
     setResume(text);
   };
@@ -17,27 +22,42 @@ export default function Body() {
     setFileText(text);
   };
 
+  const handleIdentifyKeys = () => {
+    console.log("america")
+    setShowResult(true);
+  };
+
   return (
-    <div className="p-8 h-screen bg-gradient-to-br from-purple-800 to-purple-200
-    ">
+    <div className="p-8 h-screen bg-gradient-to-br from-purple-800 to-purple-200">
       <div className="text-md">
         <div className="flex flex-row">
           <div className="w-full pr-4">
-
             <ResumeContainer>
-              <Button className= "w-full" name="Upload Resume" onFileTextChange={handleFileTextChange} />
-              {fileText && <Jobds description={fileText} />}
+              <Button
+                className="w-full"
+                name="Upload Resume"
+                onFileTextChange={handleFileTextChange}
+              />
+              {fileText && (
+                <>
+                  <Jobds description={fileText} />
+                  <ButtonSubmit text="Identify Keys" onClick={handleIdentifyKeys} />
+                </>
+              )}
             </ResumeContainer>
-           
+          </div>
+          <div className="w-full">
+            
+              <Result>
+                {showResult &&(
+                <AnalysisContainer>
+                  <SignificantTerms />
+                </AnalysisContainer>
+                )}
+              </Result>
             
           </div>
-          
-          <div className="w-full">
-            <ResumeContainer></ResumeContainer>
-          </div>
-         
         </div>
-        
       </div>
     </div>
   );
---
+++ templates/frontend/components/KeyValuePair.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from "react";
+export default function KeyValuePair(){
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    // simulating fetching data from backend
+    useEffect(() => {
+       
+        setTimeout(() => {
+          const fetchedData = [
+            { id: 1, name: 'John' },
+            { id: 2, name: 'Alice' },
+            { id: 3, name: 'Bob' },
+            { id: 1, name: 'John' },
+            { id: 2, name: 'Alice' },
+            { id: 3, name: 'Bob' },
+          ];
+          setData(fetchedData);
+          setLoading(false);
+        }, 2000); 
+      }, []);
+
+
+    return <div className="container mx-auto mt-4">
+        <table className="w-full border border-collapse">
+            <thead>
+                <tr>
+                <th className="border p-2">key</th>
+                <th className="border p-2">value</th>
+                </tr>
+            </thead>
+            <tbody>
+                {loading ? (
+                <tr>
+                    <td className="border p-2" colSpan="2">
+                    Loading...
+                    </td>
+                </tr>
+                ) : (
+                data.map((row) => (
+                    <tr key={row.id}>
+                    <td className="border p-2">{row.id}</td>
+                    <td className="border p-2">{row.name}</td>
+                    </tr>
+                ))
+                )}
+            </tbody>
+        </table>
+  </div>
+            
+}
