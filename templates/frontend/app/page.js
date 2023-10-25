d96ba3f32ed25e2988cd7bec0d6509c6a7db515c
Samet <samettaspinarx@gmail.com>
2023-10-25 09:22:55 -0400

    [FIX] removed @ sign in page.js

--- templates/frontend/app/page.js
+++ templates/frontend/app/page.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
-import Header from '@/components/Header'
-import Body from '@/components/Body'
+import Header from '/components/Header'
+import Body from '/components/Body'
 
 export default function Home() {
   return (
