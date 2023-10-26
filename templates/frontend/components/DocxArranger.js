9947bab7e536f63a8852938ce08b1622cee08d49
bereketli <blingerew2018@gmail.com>
2023-10-26 15:39:36 +0300

    [ADD] save and download buttons are added

--- templates/frontend/components/DocxArranger.js
+++ templates/frontend/components/DocxArranger.js
@@ -1,4 +1,3 @@
-"use client"
 import React, { useState, useEffect } from "react";
 import { useQuill } from "react-quilljs";
 import "quill/dist/quill.snow.css";
@@ -6,9 +5,10 @@ import mammoth from "mammoth";
 
 function DocxArranger({ setShowButton }) {
   const { quill, quillRef } = useQuill();
-  const [value, setValue] = useState("");
   const [editorVisible, setEditorVisible] = useState(false);
   const [fileContent, setFileContent] = useState(null);
+  const [editedContent, setEditedContent] = useState(null);
+  const [downloadUrl, setDownloadUrl] = useState(null);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -18,16 +18,15 @@ function DocxArranger({ setShowButton }) {
     reader.onload = (e) => {
       const fileContents = e.target.result;
 
-      // Use mammoth to convert .docx to HTML
+      
       mammoth.convertToHtml({ arrayBuffer: fileContents })
         .then((result) => {
           const html = result.value;
           setFileContent(html);
 
-          // Show the editor after the file is uploaded and processed
+    
           setEditorVisible(true);
 
-          
           setShowButton(true);
         })
         .catch((error) => {
@@ -43,6 +42,21 @@ function DocxArranger({ setShowButton }) {
     }
   }, [quill, fileContent]);
 
+
+  const handleSave = () => {
+    if (quill) {
+      const editedHTML = quill.root.innerHTML;
+      setEditedContent(editedHTML);
+
+      // Create a Blob from the edited content
+      const blob = new Blob([editedHTML], { type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document" });
+      
+      
+      const url = URL.createObjectURL(blob);
+      setDownloadUrl(url);
+    }
+  };
+
   return (
     <div className="w-full max-w-screen-xl mx-auto p-4">
       <div className="w-3/4 mx-auto my-4">
@@ -53,20 +67,33 @@ function DocxArranger({ setShowButton }) {
           className="hidden"
           id="docxInput"
         />
-        <div className="">
-        <label
-          htmlFor="docxInput"
-          className="bg-purple-900 hover-bg-blue-700 text-white py-2 px-4 rounded cursor-pointer"
-        >
-          Upload Resume
-        </label>
-
+        <div>
+          <label
+            htmlFor="docxInput"
+            className="bg-purple-900 hover-bg-blue-700 text-white py-2 px-4 rounded cursor-pointer"
+          >
+            Upload Resume
+          </label>
         </div>
-        
       </div>
+
       {editorVisible && (
-        <div className="w-3/4 mx-auto bg-white ">
-          <div style={{ height: "500px" }} ref={quillRef} />
+        <div className="w-full mx-auto bg-white relative">
+          <div className="quill-toolbar">
+            <button onClick={handleSave} className="bg-blue-500 text-white py-2 px-4 rounded mr-2">
+              Save
+            </button>
+          </div>
+          <div style={{ height: "500px", width:"650px" }} ref={quillRef} />
+          {downloadUrl && (
+            <a
+              href={downloadUrl}
+              download="edited_resume.docx"
+              className="absolute top-0 h-10 right-0 bg-green-500 text-white py-2 px-4 rounded"
+            >
+              Download File
+            </a>
+          )}
         </div>
       )}
     </div>
