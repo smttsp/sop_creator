85fa05da2565812ee72afc989e15619ff561e76b
Bereket <blingerew2018@gmail.com>
2023-10-31 08:45:32 +0300

    [UPD] upload resume fixed

--- templates/frontend/components/Body.js
+++ templates/frontend/components/Body.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Result from './Result';
 import AnalysisContainer from './AnalysisContainer';
 import SignificantTerms from './SignificantTerms';
@@ -8,7 +8,8 @@ import Button from './Button';
 
 export default function Body() {
   const [showResult, setShowResult] = useState(false);
-  const [showEditor, setShowEditor] = useState(false);
+  const [selectedFile, setSelectedFile] = useState('')
+  const fileInputRef = useRef(null);
 
   const handleIdentifyKeys = (pass) => {
     if (pass){
@@ -17,29 +18,45 @@ export default function Body() {
     
   };
 
-  const handleShowEditor = (e) => {
-    e.preventDefault();
-    setShowEditor(true);
-    console.log("showEditor"); // Log the showEditor value after it's updated
-  };
+    const handleUploadClick = () => {
+      fileInputRef.current.click();
+    };
+    const handleFileChange = (e) => {
+      const file = e.target.files[0];
+      if (file) {
+        const fileURL = URL.createObjectURL(file);
+        setSelectedFile(fileURL);
+       
+      }
+    };
+
+
 
   return (
     <div className="p-4 h-auto bg-gradient-to-br from-purple-800 to-purple-200">
       <div className="">
         <div className="grid grid-cols-3">
           <div className="w-full mx-2 pr-4 col-span-2 min-h-96">
-            <Button
+            {!selectedFile && (<Button
               text="Upload Resume"
               customClass="bg-purple-900 w-1/2 shadow-xl  
                             text-white py-2 px-4 rounded cursor-pointer"
-              onClick={handleShowEditor}
-            />
-            {showEditor && (
+              onClick={handleUploadClick}
+            />)
+           }
+            <input
+              type='file'
+              accept='.docx'
+              onChange={handleFileChange}
+              style={{ display: 'none' }}
+              ref={fileInputRef}/>
+            {selectedFile && (
               // Render the ApryseEditor component when showEditor is true
        
               <ApryseEditor text="Identify Keys" 
                             customClass ={""}
-                            shower ={handleIdentifyKeys}/>
+                            shower ={handleIdentifyKeys}
+                            selectedFile={selectedFile}/>
               
              
             )} 
