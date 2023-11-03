4a6f6a64694dd6f75c1b001096f7c831eedfcc45
bereketli <blingerew2018@gmail.com>
2023-11-03 13:47:11 +0300

    9bc72ce first version of google authentication

--- templates/frontend/components/Body.js
+++ templates/frontend/components/Body.js
@@ -7,6 +7,7 @@ import Button from './Button';
 import TextAnimation from './TextAnimation';
 
 export default function Body() {
+<<<<<<< Updated upstream
     const [showResult, setShowResult] = useState(false);
     const [selectedFile, setSelectedFile] = useState('')
     const fileInputRef = useRef(null);
@@ -72,6 +73,80 @@ export default function Body() {
                     </div>
                 </div>
             </div>
+=======
+  const [showResult, setShowResult] = useState(false);
+  const [selectedFile, setSelectedFile] = useState('');
+  const fileInputRef = useRef(null);
+
+  const handleIdentifyKeys = (pass) => {
+    if (pass) {
+      setShowResult(true);
+      scrollToMiddle(); // Scroll to the middle of the viewport when showResult is true
+    }
+  };
+
+  const handleUploadClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const fileURL = URL.createObjectURL(file);
+      setSelectedFile(fileURL);
+    }
+  };
+
+  const scrollToMiddle = () => {
+    // Calculate the position to scroll to the middle of the viewport
+    const middlePosition = window.scrollY + window.innerHeight / 2;
+
+    // Scroll to the middle of the viewport with a smooth behavior
+    window.scrollTo({
+      top: middlePosition,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className="p-4 h-auto bg-gradient-to-br from-purple-900 to-pink-200">
+      <div className="">
+        <TextAnimation text="Hello, I am an AI tool to assist you in building your career" speed={90} />
+        <div className="grid grid-cols-5">
+          <div className="w-full mx-2 pr-4 col-span-3 min-h-96">
+            {!selectedFile && (
+              <Button
+                text="Upload Resume"
+                customClass="bg-purple-900 w-1/2 shadow-xl text-white py-2 px-4 rounded cursor-pointer"
+                onClick={handleUploadClick}
+              />
+            )}
+            <input
+              type="file"
+              accept=".docx"
+              onChange={handleFileChange}
+              style={{ display: 'none' }}
+              ref={fileInputRef}
+            />
+            {selectedFile && (
+              <ApryseEditor
+                text="Identify Keys"
+                customClass={""}
+                shower={handleIdentifyKeys}
+                selectedFile={selectedFile}
+              />
+            )}
+          </div>
+          <div className="w-full col-span-2">
+            <Result className="min-h-150 h-auto">
+              {showResult && (
+                <AnalysisContainer>
+                  <SignificantTerms />
+                </AnalysisContainer>
+              )}
+            </Result>
+          </div>
+>>>>>>> Stashed changes
         </div>
     );
 }
