5efe92d97b9368712bbb15237c83a1cc79490913
Bereket <blingerew2018@gmail.com>
2023-11-03 15:10:18 +0300

    configure redux

---
+++ templates/frontend/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { auth } from './features/auth-slice';
+
+const store = configureStore({
+  reducer:{
+    
+  }
+});
+
+export default rootReducer;
