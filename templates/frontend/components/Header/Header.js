3d748378c07cb62376e1184c7b3263b31339e860
Samet <samettaspinarx@gmail.com>
2023-11-05 08:37:46 -0500

    [UPD] improved formatting

--- templates/frontend/components/Header/Header.js
+++ templates/frontend/components/Header/Header.js
@@ -1,35 +1,36 @@
 import Logo from "./Logo";
 import Navbar from "./Navbar";
 import Profile from "./Profile";
-import { useSession } from "next-auth/react";
+import {useSession} from "next-auth/react";
 import SignParent from "./SignParent";
-import { useAppSelectror } from "@/redux/store";
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { logIn } from "@/redux/features/auth-slice";
+import {useAppSelectror} from "@/redux/store";
+import {useEffect, useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {logIn} from "@/redux/features/auth-slice";
 
 export default function Header() {
-  const [user, setUser] = useState({});
-  const { data: session } = useSession();
-  const authReducerValue = useAppSelectror((state) => state.authReducer.value);
-  const dispatch = useDispatch();
+    const [user, setUser] = useState({});
+    const {data: session} = useSession();
+    const authReducerValue = useAppSelectror((state) => state.authReducer.value);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (session && !user.isAuth) {
-      dispatch(logIn({ name: session.user.name, image: session.user.image }));
-    }
-  }, [session, user]);
+    useEffect(() => {
+        if (session && !user.isAuth) {
+            dispatch(logIn({name: session.user.name, image: session.user.image}));
+        }
+    }, [session, user]);
 
-  useEffect(() => {
-    setUser(authReducerValue);
-  }, [authReducerValue]);
+    useEffect(() => {
+        setUser(authReducerValue);
+    }, [authReducerValue]);
 
-  return (
-    <div className="bg-purple-900 font-roboto flex justify-between items-center py-1 px-4 w-full h-16 pd-y-2 pd-x-6">
-      <Logo />
-      <Navbar />
-      <SignParent />
-      {user.isAuth && (<Profile user={user} />)}
-    </div>
-  );
+    return (
+        <div
+            className="bg-purple-900 font-roboto flex justify-between items-center py-1 px-4 w-full h-16 pd-y-2 pd-x-6">
+            <Logo/>
+            <Navbar/>
+            <SignParent/>
+            {user.isAuth && (<Profile user={user}/>)}
+        </div>
+    );
 }
--- templates/frontend/components/Header/Inpupt.js
+++ templates/frontend/components/Header/Inpupt.js
@@ -1,14 +1,22 @@
-import Link from "next/link"
-const Input = (props) =>{
-    return(
-       
-        <div className = "relative mt-8 text-sm text-gray-800">
-            <input type={props.type} id = {props.id} value = {props.value} onChange={props.onChange} name = {props.name} placeholder={props.placeholder}
-            className = "peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 text-sm font-light placeholder:text-transparent focus:border-gray-500 focus:outline-none"  autocomplete="NA" />
-            
-            <label className = "pointer-events-none font-md absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-700" >{props.lable}</label>
+const Input = (props) => {
+    return (
+
+        <div className="relative mt-8 text-sm text-gray-800">
+            <input type={props.type} id={props.id} value={props.value} onChange={props.onChange}
+                   name={props.name} placeholder={props.placeholder}
+                   className="peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 text-sm
+                   font-light placeholder:text-transparent focus:border-gray-500 focus:outline-none"
+                   autocomplete="NA"/>
+            <label
+                className="pointer-events-none font-md absolute top-0 left-0 origin-left
+                -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all
+                duration-100 ease-in-out peer-placeholder-shown:top-1/2
+                peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500
+                peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-700">
+                {props.lable}
+            </label>
         </div>
-        
+
     )
 }
 
