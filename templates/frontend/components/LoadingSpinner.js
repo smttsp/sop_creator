b19de840296261bd2ce3c514de54d4d8e139e92e
Bereket <blingerew2018@gmail.com>
2023-11-09 12:26:51 +0300

    [ADD] add spiner to AI analysis

--- templates/frontend/components/LoadingSpinner.js
+++ templates/frontend/components/LoadingSpinner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingSpinner = ({customClass}) => {
     return (
-        <div className="flex justify-center items-center h-full">
+        <div className="flex mx-auto justify-center items-center h-full">
             <div
                 className={`${customClass} border-t-4 border-purple-900 border-solid rounded-full animate-spin`}></div>
         </div>
