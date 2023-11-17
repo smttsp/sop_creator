1f03b51046094bdc9875aa3bb21503d71989857b
Bereket <blingerew2018@gmail.com>
2023-11-17 15:49:44 +0300

    [UPD] extend session expiration time to one week

--- templates/frontend/app/api/auth/[...nextauth]/route.js
+++ templates/frontend/app/api/auth/[...nextauth]/route.js
@@ -1,34 +1,57 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            authorization: {
-                params: {
-                    scope: "openid email profile https://www.googleapis.com/auth/drive",
-                },
+import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth";
+
+const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "openid email profile https://www.googleapis.com/auth/drive",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.expires = account.expires_in + 3600 * 24 * 7; 
+      }
+      if (token.expires && Date.now() / 1000 - token.expires > -300) {
+        try {
+          const refreshedData = await fetch("https://oauth2.googleapis.com/token", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
             },
-        }),
-    ],
-    callbacks: {
-        async jwt({token, account, profile}) {
-            // Persist the OAuth access_token and or the user id to the token right after signin
-            if (account) {
-                token.accessToken = account.access_token;
-            }
-            return token;
-        },
-        async session({session, token, user}) {
-            // Send properties to the client, like an access_token and user id from a provider.
-            session.accessToken = token.accessToken;
-            return session;
-        },
+            body: new URLSearchParams({
+              refresh_token: account.refresh_token,
+              client_id: process.env.GOOGLE_CLIENT_ID,
+              client_secret: process.env.GOOGLE_CLIENT_SECRET,
+              grant_type: "refresh_token",
+            }),
+          });
+          const refreshedToken = await refreshedData.json();
+
+          token.accessToken = refreshedToken.access_token;
+          token.expires = refreshedToken.expires_in + 3600 * 24 * 7; 
+        } catch (error) {
+          console.error("Error refreshing token:", error);
+        }
+      }
+
+      return token;
     },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
