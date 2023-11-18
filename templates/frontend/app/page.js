e4d75759d2a939ca8dddb9056ca958d579bb4ce4
Bereket <blingerew2018@gmail.com>
2023-11-18 10:57:12 +0300

    [UPD] arrange the structure

--- templates/frontend/app/page.js
+++ templates/frontend/app/page.js
@@ -1,11 +1,10 @@
-import Body from '@/components/LandingPage/body'
-
+import BodyLandingPage from "@/components/LandingPage/BodyLandingPage"
 
 export default function Home() {
     return (
         <main className="relative overflow-hidden bg-white">
             <div className=''>
-                <div className=''><Body/></div>
+                <div className=''><BodyLandingPage/></div>
             </div>
         </main>
     )
