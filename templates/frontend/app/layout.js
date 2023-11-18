9f3615e8e702d7e3b55a6ca9a6c29c6f7f7adcd8
Bereket <blingerew2018@gmail.com>
2023-11-18 10:10:50 +0300

    [CLN] remove uncessary dependencies

--- templates/frontend/app/layout.js
+++ templates/frontend/app/layout.js
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-white`}>
         <NextAuthProvider>
           <Header />
           {children}
